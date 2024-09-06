3fd3be275368018ddb90fc6deffe030d41a6307b
Matthias Ruf <Matthias@ruf-media.com>
2024-09-06 09:28:01 +0200

    updatestammdaten

--- pages/api/updateStammdaten.tsx
+++ pages/api/updateStammdaten.tsx
@@ -1,7 +1,5 @@
-// pages/api/uploadStammdaten.ts
-
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Standort } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -23,37 +21,45 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     linkedin,
     xing,
     status,
-    kategorie,
+    kategorie, // Hier wird der Wert von der Anfrage empfangen
     zentraleMail,
     zentralTelefon,
-    hauptansprechpartner,
     vermittlungsprovision,
-    vermittlungsprovisionIntervall,
     usbBeschreibung,
     interneNotizen,
   } = req.body;
 
   try {
+    // Umwandlung des `kategorie`-Werts in eine Ganzzahl oder Setzen auf `undefined`, wenn der Wert nicht da ist
+    const kategorieValue: number | undefined = kategorie ? parseInt(kategorie, 10) : undefined;
+
+    // Prisma erwartet keinen `null`-Wert für `number`, sondern entweder eine Zahl oder `undefined`
+    const updateData: any = {
+      name,
+      strasse,
+      postleitzahl,
+      stadt,
+      standort: standort || null,
+      homepage,
+      jobsite,
+      linkedin,
+      xing,
+      status,
+      zentraleMail,
+      zentralTelefon,
+      vermittlungsprovision,
+      usbBeschreibung,
+      interneNotizen,
+    };
+
+    // Nur wenn `kategorie` gesetzt ist, fügen wir es zum `data`-Objekt hinzu
+    if (kategorieValue !== undefined) {
+      updateData.kategorie = kategorieValue;
+    }
+
     const updatedUnternehmen = await prisma.unternehmen.update({
       where: { id },
-      data: {
-        name,
-        strasse,
-        postleitzahl,
-        stadt,
-        standort,
-        homepage,
-        jobsite,
-        linkedin,
-        xing,
-        status,
-        kategorie,
-        zentraleMail,
-        zentralTelefon,
-        vermittlungsprovision,
-        usbBeschreibung,
-        interneNotizen,
-      },
+      data: updateData,
     });
 
     res.status(200).json(updatedUnternehmen);
