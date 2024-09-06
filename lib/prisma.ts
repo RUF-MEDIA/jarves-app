2a384e6c806ba68325936eaff353d0bfb35d23a8
Matthias Ruf <Matthias@ruf-media.com>
2024-09-06 08:18:21 +0200

    new

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,50 +1,17 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const prismaOptions: Prisma.PrismaClientOptions = {
-  log: [
-    { emit: 'stdout', level: 'query' },
-    { emit: 'stdout', level: 'info' },
-    { emit: 'stdout', level: 'warn' },
-    { emit: 'stdout', level: 'error' },
-  ],
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-class PrismaManager {
-  private static instance: PrismaClient;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-  static getInstance(): PrismaClient {
-    if (!PrismaManager.instance) {
-      PrismaManager.instance = new PrismaClient(prismaOptions);
-    }
-    return PrismaManager.instance;
-  }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-  static async disconnect() {
-    if (PrismaManager.instance) {
-      await PrismaManager.instance.$disconnect();
-    }
-  }
-}
-
-const prisma = PrismaManager.getInstance();
-
-// Verbindungstest (Sie können diesen Block entfernen, nachdem Sie die Verbindung getestet haben)
-prisma
-  .$connect()
-  .then(() => console.log('Database connection successful'))
-  .catch((e) => console.error('Database connection failed', e));
-
-if (process.env.NODE_ENV !== 'production') {
-  (global as any).prisma = prisma;
-}
-
-process.on('beforeExit', async () => {
-  await PrismaManager.disconnect();
-});
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
